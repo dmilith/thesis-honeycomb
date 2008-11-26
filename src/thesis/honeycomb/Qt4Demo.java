62d4231be8b2cef5b3c9baf27523b95a4c0fcded
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2008-11-26 01:15:35 +0100

    added mouse listener, fixed segv. added vectors as element containers

--- src/thesis/honeycomb/Qt4Demo.java
+++ src/thesis/honeycomb/Qt4Demo.java
@@ -1,5 +1,6 @@
 package thesis.honeycomb;
 
+import java.util.Vector;
 import com.trolltech.qt.core.*;
 import com.trolltech.qt.gui.*;
 import com.trolltech.qt.opengl.QGLWidget;
@@ -12,23 +13,44 @@ import com.trolltech.qt.opengl.QGLWidget;
 
 public class Qt4Demo extends QGLWidget {
 
-  static QPixmap myImage;
+  private QPixmap myImage;
+  private QPainter painter;
   private final int WIDTH = 800, HEIGHT = 600;
-  private QMovie movie;
+  private QMovie movie, movie2, movie3;
+  private QFont font;
+  private Vector<QLabel> placeContainer;
+  private Vector<QPixmap> pixmapContainer;
 
   Qt4Demo() {
+    super();
+    placeContainer = new Vector<QLabel>();
+    pixmapContainer = new Vector<QPixmap>();
+
     // this isn't required but may be useful
     QTextCodec.setCodecForCStrings( QTextCodec.codecForName( "UTF-8" ) );
 
-    myImage = new QPixmap();
-    myImage.load( "images/DoctorSaid.jpg" );
+    pixmapContainer.add( 0, new QPixmap("images/alco.jpg") );
+    pixmapContainer.add( 1, new QPixmap("images/fire.gif") );
 
-    QLabel label = new QLabel();
-    movie = new QMovie("images/fire.gif");
-    label.setMovie(movie);
+    for ( int i = 0; i < 10; i++ )
+      placeContainer.add( i, new QLabel( this ) );
+
+    for ( int i = 0; i < placeContainer.size(); i++ ) {
+      placeContainer.elementAt(i).setPixmap( pixmapContainer.elementAt(0) );
+      placeContainer.elementAt(i).move( 100*i, 200 );
+    }
+
+   /*
+    QLabel label = new QLabel( this );
+    movie = new QMovie( "images/fire.gif" );
+    label.setMovie( movie );
+    label.move( 100, 100 );
+    movie.setSpeed( 40 );
+    movie.setCacheMode( QMovie.CacheMode.CacheAll );
     movie.start();
-    label.setParent( this );
+    */
 
+    font = new QFont( "Times", 17 );
     setWindowTitle( "Thesis HoneyComb" );
     setGeometry( 0, 0, WIDTH, HEIGHT );
     setMaximumHeight( HEIGHT );
@@ -40,7 +62,7 @@ public class Qt4Demo extends QGLWidget {
 
   @Override
   public void paintEvent( QPaintEvent event ) {
-    QPainter painter = new QPainter();
+    painter = new QPainter();
     painter.begin( this );
 
     painter.setBackgroundMode( Qt.BGMode.OpaqueMode );
@@ -51,22 +73,24 @@ public class Qt4Demo extends QGLWidget {
     painter.setClipping( true );
     painter.setRenderHint( QPainter.RenderHint.Antialiasing );
     painter.setRenderHint( QPainter.RenderHint.TextAntialiasing );
-    QFont font = new QFont( "Times", 17 );
     painter.setFont( font );
-    painter.drawImage( 50, 50, myImage.toImage(), 0 );
+    //  will cause SIGSEGV here:  painter.drawPixmap( 50, 50, myImage );
     painter.drawText( 600, 50, "Cześć załogo z utf8" );
     painter.drawLine( 100, 100, 620, 550 );
     painter.drawLine( 100, 100, 690, 460 );
     painter.drawLine( 690, 460, 620, 550 );
-
+    
     painter.end();
+  }
+
+  @Override
+  public void mousePressEvent( QMouseEvent event ) {
+    System.out.println( "Pozycja myszki w oknie: x:" + event.pos().x() + " - y:" + event.pos().y() );
   }
 
   public static void main( String args[] ) {
     QApplication.initialize( args );
-
-    new Qt4Demo();
-
+      Qt4Demo z = new Qt4Demo();
     QApplication.exec();
   }
 
