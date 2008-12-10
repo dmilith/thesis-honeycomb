d3e1c83b3b82566c0611a06bf27d27df144e8778
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2008-12-10 18:58:37 +0100

    added own slot to qt4Demo2

--- src/thesis/honeycomb/Qt4Demo2.java
+++ src/thesis/honeycomb/Qt4Demo2.java
@@ -11,14 +11,30 @@ import com.trolltech.qt.gui.*;
  *
  * @author dmilith
  */
-public class Qt4Demo2 {
+public class Qt4Demo2 extends QMainWindow {
+
+  public void nameHere() {
+    System.out.println("Kliknięty");
+  }
+
+/*
+  loop = new GameLoop();
+
+  Thread thread = new Thread(loop);
+  loop.moveToThread(thread);
+  */
 
   public static void main( String[] args ) {
     QApplication.initialize(args);
-    QLineEdit edit = new QLineEdit( "Loren ipsum" );
-    edit.show();
-    edit.textChanged.connect( System.out, "println(String)" );
-    edit.textChanged.connect( edit, "setWindowTitle(String)" );
+      Qt4Demo2 widget = new Qt4Demo2();
+      QLineEdit edit = new QLineEdit( "Loren ipsum" );
+      edit.setParent( widget );
+      QPushButton button = new QPushButton();
+      button.pressed.connect( widget, "nameHere()" );
+      button.setParent( widget );
+      edit.textChanged.connect( System.out, "println(String)" );
+      edit.textChanged.connect( edit, "setWindowTitle(String)" );
+      widget.show();
     QApplication.exec();
   }
   
