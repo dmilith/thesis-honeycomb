ce3f4f6f3b1b1aa90a5b60fe63f03b2dd7a7996c
Daniel (dmilith) Dettlaff <dmilith@drakor.eu>
2008-12-11 17:32:46 +0100

    svg Comb as Widget which accepts underMouse() event!

--- src/thesis/honeycomb/Hexagon.java
+++ src/thesis/honeycomb/Hexagon.java
@@ -1,7 +1,6 @@
 package thesis.honeycomb;
 
-import com.trolltech.qt.opengl.QGLWidget;
-import com.trolltech.qt.svg.QSvgRenderer;
+import com.trolltech.qt.svg.QSvgWidget;
 
 /**
  *
@@ -9,51 +8,44 @@ import com.trolltech.qt.svg.QSvgRenderer;
  * Created on Nov 25, 2008, 5:55:42 PM
  */
 
-public class Hexagon {
+public class Hexagon extends QSvgWidget {
 
   private final double a = 34;
   private final double h = a * (Math.sqrt(3) / 2);
   private final double l = 2 * h;
  
-  private QSvgRenderer hexagon;
-  private double x, y;
+  private double vX, vY;
 
-  Hexagon( double _vectorX, double _vectorY, QGLWidget _parent ) {
-    super();
-    hexagon = new QSvgRenderer( "classpath:images/hexagon.svg", _parent );
-    x = _vectorX - a;
-    y = _vectorY - h;
+  Hexagon( double _vectorX, double _vectorY ) {
+    super( "classpath:images/hexagon.svg" );
+    this.setMaximumWidth((int)a);
+    this.setMaximumHeight((int)a);
+    this.setMaximumSize((int)a, (int)a);
+    vX = _vectorX - a;
+    vY = _vectorY - h;
   }
 
-  Hexagon( double _x, double _y, QGLWidget _parent, String _file_name ) {
-    super();
-    hexagon = new QSvgRenderer( "classpath:images/" + _file_name, _parent );
-    x = _x - a;
-    y = _y - h;
+  Hexagon( double _x, double _y, String _file_name ) {
+    super( "classpath:images/" + _file_name );
+    this.setFixedSize((int)a, (int)a);
+    vX = _x - a;
+    vY = _y - h;
   }
 
-  public QSvgRenderer getHexagon() {
-    return hexagon;
+  public double vX() {
+    return vX;
   }
 
-  public void setHexagon(QSvgRenderer hexagon) {
-    this.hexagon = hexagon;
+  public void setVX(double vX) {
+    this.vX = vX;
   }
 
-  public double x() {
-    return x;
+  public double vY() {
+    return vY;
   }
 
-  public void setX(double x) {
-    this.x = x;
-  }
-
-  public double y() {
-    return y;
-  }
-
-  public void setY(double y) {
-    this.y = y;
+  public void setVY(double vY) {
+    this.vY = vY;
   }
 
 }
--- src/thesis/honeycomb/HexagonGenerator.java
+++ src/thesis/honeycomb/HexagonGenerator.java
@@ -6,9 +6,10 @@
 package thesis.honeycomb;
 
 import com.trolltech.qt.gui.*;
-import com.trolltech.qt.svg.QSvgRenderer;
+import com.trolltech.qt.svg.QSvgWidget;
 import com.trolltech.qt.opengl.QGLWidget;
 import com.trolltech.qt.core.*;
+import com.trolltech.qt.svg.QSvgRenderer;
 
 /**
  *
@@ -17,14 +18,17 @@ import com.trolltech.qt.core.*;
 public class HexagonGenerator extends QGLWidget {
 
   private QPainter painter;
-  private QSvgRenderer svgImg;
   private Hexagon myHex;
+  private int mouseX, mouseY, choosenObject = 0;
+
+  private final int plusX = 250, plusY = 250;
 
   HexagonGenerator() {
     super();
+    myHex = new Hexagon( 0, 0 );
+    myHex.setParent(this);
+    this.setGeometry(0, 0, plusX*2, plusY*2);
     this.show();
-    myHex = new Hexagon( 0, 0, this );
-    svgImg = myHex.getHexagon();
   }
 
   @Override
@@ -36,13 +40,32 @@ public class HexagonGenerator extends QGLWidget {
   }
 
   public void paint( QPainter painter ) {
-    svgImg.render( painter, new QRectF(100, 100, 34, 34 ) );
+    myHex.render(painter, new QPoint(0,0));
   }
 
   public static void main(String[] args) {
     QApplication.initialize(args);
     new HexagonGenerator();
     QApplication.exec();
+  }
+
+  @Override
+  public void mousePressEvent( QMouseEvent event ) {
+    if ( event.button().equals( event.button().LeftButton ) ) {
+      System.out.println( "Pozycja myszki w oknie: x:" + event.pos().x() + " - y:" + event.pos().y() );
+      mouseX = event.pos().x();
+      mouseY = event.pos().y();
+      repaint();
+    } else if ( event.button().equals( event.button().RightButton ) ) {
+      for ( int i = 0; i < 1; i++ ) {
+        if ( ( myHex.underMouse() ) && ( event.button().equals( event.button().RightButton )) ) {
+          System.out.println( "Kliknąłeś na obiekt numer :" + i + "\nWybrano obiekt.");
+          choosenObject = i;
+        }
+      }
+    } else if ( event.button().equals( event.button().MidButton ) ) {
+      System.out.println( "Wciśnięto środkowy przycisk myszy");
+    }
   }
 
 }
